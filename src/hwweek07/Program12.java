daf6809ab6970addd14ecfaf4bc5270e01d795f2
Londonboy017 <100875957+Londonboy017@users.noreply.github.com>
2022-03-22 22:59:31 +0000

    Update Program12.java
    made changes on line 11.

--- src/hwweek07/Program12.java
+++ src/hwweek07/Program12.java
@@ -8,7 +8,7 @@ public class Program12 {
         char x =obj.next().charAt(0);//it will consider first letter of input
         if ((x>='a'&&x<='z')||(x>='A'&&x<='Z')){
             System.out.println(x+" is a Alphabet");
-        }else if(x>=0&&x<=9){
+        }else if(x>='0'&&x<='9'){
             System.out.println(x+ " is a Number");
         }else{
             System.out.println(x+ " is a Symbol");
